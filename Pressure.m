5228eb55d8c8e1b658c3f7fc64ecde4e24d0d2c0
Ben Smith <29705711+bensmith95@users.noreply.github.com>
2022-07-10 13:51:27 +0100

    New Pressure function
    Added pressure inlet from boundary layer
    Removed need to solve on staggered grid i.e. uses same grid as velocities

--- Pressure.m
+++ Pressure.m
@@ -1,44 +1,63 @@
 %% Determine Pressure distribution
 % via the Pressure Laplace equation
-function P = Pressure(U,V,Re,h)
+function P = Pressure(U,W,Re,h)
     %% Initialise
-    [Ny,Nx] = size(U); hi = 1/h;
+    [Nbeta,Neta] = size(U); hi = 1/h;
     
-    % map to staggered grid
-    Us1 = (U(1:Ny-1,:)+U(2:Ny,:))*0.5; Vs1 = (V(:,1:Nx-1)+V(:,2:Nx))*0.5;
-    Vs2 = (V(1:Ny-1,:)+V(2:Ny,:))*0.5; Us2 = (U(:,1:Nx-1)+U(:,2:Nx))*0.5;
+    % load Pressure inlet
+    filename = '../Flows/Stew_BL.mat';
+    load(filename,'VelBL','eta','theta'); PB = VelBL{4}; Eta = eta; eta = 0:h:30;
+    % Determine betamax in theta
+    Tm = -10/Re + pi/2; [~,i] = min(abs(theta-Tm));
+    % map to current grid
+    PI = spline(Eta,PB(i,:),eta); % P in
+    
+   % determine derivatives
+    dUdeta = zeros(Nbeta,Neta); dUdbeta = dUdeta; 
+    dWdeta = dUdeta; dWdbeta = dUdeta; 
+    % U
+    dUdeta(:,1) = -(3*U(:,1)-4*U(:,2)+U(:,3))*0.5*hi;
+    dUdeta(:,2:Neta-1) = (U(:,3:Neta)-U(:,1:Neta-2))*0.5*hi; 
+    dUdeta(:,Neta) = (3*U(:,Neta)-4*U(:,Neta-1)+U(:,Neta-2))*0.5*hi;
+    dUdbeta(1,:) = -(3*U(1,:)-4*U(2,:)+U(3,:))*0.5*hi;
+    dUdbeta(2:Nbeta-1,:) = (U(3:Nbeta,:)-U(1:Nbeta-2,:))*0.5*hi;
+    dUdbeta(Nbeta,:) = (3*U(Nbeta,:)-4*U(Nbeta-1,:)+U(Nbeta-2,:))*0.5*hi;
+    % W
+    dWdeta(:,1) = -(3*W(:,1)-4*W(:,2)+W(:,3))*0.5*hi;
+    dWdeta(:,2:Neta-1) = (W(:,3:Neta)-W(:,1:Neta-2))*0.5*hi; 
+    dWdeta(:,Neta) = (3*W(:,Neta)-4*W(:,Neta-1)+W(:,Neta-2))*0.5*hi;
+    dWdbeta(1,:) = -(3*W(1,:)-4*W(2,:)+W(3,:))*0.5*hi;
+    dWdbeta(2:Nbeta-1,:) = (W(3:Nbeta,:)-W(1:Nbeta-2,:))*0.5*hi;
+    dWdbeta(Nbeta,:) = (3*W(Nbeta,:)-4*W(Nbeta-1,:)+W(Nbeta-2,:))*0.5*hi;
 
     % determine f
-    dUdx = (Us1(:,2:end)-Us1(:,1:end-1))*0.5*hi; dUdy = (Us2(2:end,:)-Us2(1:end-1,:))*0.5*hi; 
-    dVdx = (Vs2(:,2:end)-Vs2(:,1:end-1))*0.5*hi; dVdy = (Vs1(2:end,:)-Vs1(1:end-1,:))*0.5*hi;
-    F = 2*Re*(dUdx.*dVdy - dVdx.*dUdy)*h^2; Nx = Nx-1; Ny = Ny-1;
+    F = 2*Re*(dWdeta.*dUdbeta - dUdeta.*dWdbeta)*h^2; 
+    F(Nbeta,:) = PI;
+    
 
     %% Laplace operator 
-    e = ones(Nx,1); 
-    D = spdiags([e,-4*e,e],-1:1,Nx,Nx); D(1,1) = -3; D(Nx,Nx) = -3;
-    Db = spdiags([e,-3*e,e],-1:1,Nx,Nx); Db(1,1) = -2; Db(Nx,Nx) = -2;
-    I = speye(Nx);
-    Z = sparse(Nx,Nx); 
-    A = [Db, I, repmat(Z,[1,Ny-2])];
-    for j = 2:Ny-1
-        A = [A; repmat(Z,[1,j-2]), I, D, I, repmat(Z,[1,Ny-3-j+2])];
+    e = ones(Neta,1); 
+    D = spdiags([e,-4*e,e],-1:1,Neta,Neta); D(1,2) = 2; D(Neta,Neta-1) = 2;
+    I = speye(Neta);
+    Z = sparse(Neta,Neta); 
+    A = [D, 2*I, repmat(Z,[1,Nbeta-2])];
+    for j = 2:Nbeta-1
+        A = [A; repmat(Z,[1,j-2]), I, D, I, repmat(Z,[1,Nbeta-3-j+2])];
     end
-    A = [A; repmat(Z,[1,Ny-2]), I, Db];
-    A(1,1) = 1; A(1,2:end) = 0;
+    A = [A; repmat(Z,[1,Nbeta-1]), I];
 
     % reorganise B into a vector
-    f = zeros(Nx*Ny,1);
-    for j=1:Ny
-       f(1+(j-1)*Nx:j*Nx) = F(j,:); 
+    f = zeros(Neta*Nbeta,1);
+    for j=1:Nbeta
+       f(1+(j-1)*Neta:j*Neta) = F(j,:); 
     end
-    f(1) = 1;
 
     %% Solve linear system
     Pv = A\f;
-    
+
     % reorganise solution into matrix form
-    P = zeros(Ny,Nx);
-    for j=1:Ny
-       P(j,:) = Pv(1+(j-1)*Nx:j*Nx); 
+    P = zeros(Nbeta,Neta);
+    for j=1:Nbeta
+        P(j,:) = Pv(1+(j-1)*Neta:j*Neta); 
     end
 end
